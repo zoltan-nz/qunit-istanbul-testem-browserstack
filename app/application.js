40625a87a5304ad3e636ebd6ef6697747a800268
Zoltan <zdebre@gmail.com>
2014-08-08 19:01:46 +1200

    Logging variables

--- app/application.js
+++ app/application.js
@@ -5,15 +5,18 @@ var App = Ember.Application.extend({
   rootElement:      '#ember-app',
   modulePrefix:     'app',
   Resolver:         Resolver
+  //,LOG_TRANSITIONS_INTERNAL:  true
+  //,LOG_ACTIVE_GENERATION:     true
+  //,LOG_VIEW_LOOKUPS:          true
+  //,LOG_RESOLVER:              true
 });
 
 loadInitializers(App, 'app');
 
-Ember.run.backburner.DEBUG = true;
-Ember.ENV.RAISE_ON_DEPRECATION = true;
-Ember.LOG_STACKTRACE_ON_DEPRECATION = true;
-Ember.LOG_MODULE_RESOLVER = true;
-Ember.LOG_TRANSITIONS_INTERNAL = true;
-Ember.LOG_ACTIVE_GENERATION = true;
-Ember.LOG_VIEW_LOOKUPS = true;
-Ember.LOG_BINDINGS = true;
+//Ember.run.backburner.DEBUG            = true;
+//Ember.ENV.RAISE_ON_DEPRECATION        = true;
+//Ember.LOG_STACKTRACE_ON_DEPRECATION   = true;
+//Ember.LOG_BINDINGS                    = true;
+//Ember.RSVP.on('error', function(error) {
+//  Ember.Logger.assert(false, error);
+//});
