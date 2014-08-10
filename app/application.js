7150883019f881afbc07a433b67f83a9ddd7fc9a
Zoltan <zdebre@gmail.com>
2014-08-10 23:07:16 +1200

    Add public

--- app/application.js
+++ app/application.js
@@ -5,18 +5,18 @@ var App = Ember.Application.extend({
   rootElement:      '#ember-app',
   modulePrefix:     'app',
   Resolver:         Resolver
-  //,LOG_TRANSITIONS_INTERNAL:  true
-  //,LOG_ACTIVE_GENERATION:     true
-  //,LOG_VIEW_LOOKUPS:          true
-  //,LOG_RESOLVER:              true
+  ,LOG_TRANSITIONS_INTERNAL:  true
+  ,LOG_ACTIVE_GENERATION:     true
+  ,LOG_VIEW_LOOKUPS:          true
+  ,LOG_RESOLVER:              true
 });
 
 loadInitializers(App, 'app');
 
-//Ember.run.backburner.DEBUG            = true;
-//Ember.ENV.RAISE_ON_DEPRECATION        = true;
-//Ember.LOG_STACKTRACE_ON_DEPRECATION   = true;
-//Ember.LOG_BINDINGS                    = true;
-//Ember.RSVP.on('error', function(error) {
-//  Ember.Logger.assert(false, error);
-//});
+Ember.run.backburner.DEBUG            = true;
+Ember.ENV.RAISE_ON_DEPRECATION        = true;
+Ember.LOG_STACKTRACE_ON_DEPRECATION   = true;
+Ember.LOG_BINDINGS                    = true;
+Ember.RSVP.on('error', function(error) {
+  Ember.Logger.assert(false, error);
+});
