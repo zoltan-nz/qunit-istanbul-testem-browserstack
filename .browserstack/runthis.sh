85d73afc1b136b13a3da71827da43a380e9385ee
Zoltan <zdebre@gmail.com>
2014-08-11 10:24:30 -0300

    Add BrowserStackLocal

--- .browserstack/runthis.sh
+++ .browserstack/runthis.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo "{\"username\":\"`echo $BS_USER`\", \"password\":\"`echo $BS_PASSWORD`\", \"privateKey\": \"`echo $BS_KEY`\", \"apiKey\":\"`echo $BS_KEY`\"}" >> ~/.browserstack/browserstack.json
+./.browserstack/BrowserStackLocal $BS_KEY &
