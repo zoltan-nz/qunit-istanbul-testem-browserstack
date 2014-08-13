fa9a9e1ad9546f7479b487ab834d2c33ca7ecc82
Zoltan <zdebre@gmail.com>
2014-08-13 22:43:35 +1200

    Huge changes

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+var App = window.App = Ember.Application.create();
+
+/* Order and include as you please. */
+require('scripts/controllers/*');
+require('scripts/store');
+require('scripts/models/*');
+require('scripts/routes/*');
+require('scripts/components/*');
+require('scripts/views/*');
+require('scripts/router');
+
+console.log(App);
